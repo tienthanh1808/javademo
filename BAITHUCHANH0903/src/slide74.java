8504c10475c6357217eefbd8bc6f307acfdf0eaa
tienthanh1808 <tienthanh2k3vh@gmail.com>
2023-03-13 10:34:11 +0700

    first commit

---
+++ BAITHUCHANH0903/src/slide74.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class slide74 {
+    public static void maid(String[] args) {
+       {  Scanner sc = new Scanner(System.in);
+    int n;
+         System.out.println("nhap vao kich thuc mang")
+    n = sc.nextTnt()
+       }while(n<=0)
+       int arrA[]= new int[n];
+       for(int=0; i<arrA,length;i++)
+       {
+        
+        System.out.printf(format:"arrA[%d]=",i);
+        arrA[i]=sc.nextTnt();
+
+       }
+       for(int i = 0; i< arrA.length;i++)
