afa1d3e4178cc8c528d2ed70f2d11f3c83d9709f
tienthanh1808 <tienthanh2k3vh@gmail.com>
2023-03-16 13:16:10 +0700

    first commit

--- BAITHUCHANH0903/src/slide74.java
+++ BAITHUCHANH0903/src/slide74.java
@@ -1,18 +1,26 @@
 import java.util.Scanner;
 
 public class slide74 {
-    public static void maid(String[] args) {
-       {  Scanner sc = new Scanner(System.in);
-    int n;
-         System.out.println("nhap vao kich thuc mang")
-    n = sc.nextTnt()
-       }while(n<=0)
-       int arrA[]= new int[n];
-       for(int=0; i<arrA,length;i++)
-       {
-        
-        System.out.printf(format:"arrA[%d]=",i);
-        arrA[i]=sc.nextTnt();
 
-       }
-       for(int i = 0; i< arrA.length;i++)
+   public static void main(String[] args) {
+      int n;
+      Scanner sc = new Scanner(System.in);
+      do {
+         System.out.println("nhap vao kich thuoc cua mang");
+         n = sc.nextInt();
+      } while (n <= 0);
+      int arrA[] = new int[n];
+      for (int i = 0; i < n; i++) {
+         System.out.printf("arrA[%d]= ", i);
+         arrA[i] = sc.nextInt();
+      }
+      int tong = 0;
+      for (int i = 0; i < n; i++) {
+         if (arrA[i] % 2 == 0) {
+            System.out.printf("arrA[%d]+", i);
+            tong += arrA[i];
+         }
+      }
+      System.out.print("= " + tong);
+   }
+}
